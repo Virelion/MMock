185575a150d3c9b9978a785c936075ad54fec3ce
Maciej Ziemba <m.ziemba95@gmail.com>
2020-09-26 19:22:35 +0200

    New repository location notice

--- integTest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./gradlew publishToMavenLocal -Pversion=0.0.0-SNAPSHOT
-cd demo-project
-./gradlew clean check
--- settings.gradle.kts
+++
@@ -1,9 +0,0 @@
-rootProject.name = "mmock"
-
-include(":mmock-runtime")
-include(":mmock-annotations")
-include(":mmock-compiler-plugin")
-include(":mmock-compiler-plugin-native")
-include(":mmock-gradle-plugin")
-
-enableFeaturePreview("GRADLE_METADATA")
